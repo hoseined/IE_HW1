a31ffb7c29a73a60174f7303e4c3605152ecc053
hoseined <ho3ined.he@gmail.com>
2019-10-25 19:00:10 +0330

    fixed the req.body problem

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var insidePolygon = require('geolocation-utils').insidePolygon
+var bodyparser = require('body-parser');
 var temp_name_db = 'db.json';
 var local_database;
 const app = express();
+app.use(express.json());
 
 fs.readFile(path.resolve(__dirname, temp_name_db), 'UTF-8', (err, data) => {
     if (!err){
@@ -17,36 +19,38 @@ fs.readFile(path.resolve(__dirname, temp_name_db), 'UTF-8', (err, data) => {
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('server is running on port 3000')
-    app.get('/gis/testpoint/:lat/:lon/', (req, res, next) => {
-        var coordinates = [parseFloat(req.params.lat, 10), parseFloat(req.params.lon, 10)]
-        var result = { 'polygons': []}
-        for (var obj of local_database['features']){
-                if (obj['geometry']['type'] === 'Polygon'){
-                var polygon = obj['geometry']['coordinates'][0]
-                if (insidePolygon(coordinates, polygon)){
-                    result['polygons'].push(obj['properties'])
-                }
+app.put('/gis/addpolygon/', (req, res, next) => {
+    console.log(req.body)
+    local_database['features'].push(req.body);
+    fs.writeFile(path.resolve(__dirname, '../'+temp_name_db), JSON.stringify(local_database), (err) => {
+        if (err){
+        console.log('wrong data format');
+        }
+        else{
+        console.log("data saved to 'database'.")
+        }
+    })
+    res.send(local_database);
+});
+
+app.get('/gis/testpoint/:lat/:lon/', (req, res, next) => {
+    var coordinates = [parseFloat(req.params.lat, 10), parseFloat(req.params.lon, 10)]
+    var result = { 'polygons': []}
+    for (var obj of local_database['features']){
+            if (obj['geometry']['type'] === 'Polygon'){
+            var polygon = obj['geometry']['coordinates'][0]
+            if (insidePolygon(coordinates, polygon)){
+                result['polygons'].push(obj['properties'])
             }
         }
-        res.send(result);
-    });
-
-    app.put('/gis/addpolygon/', (req, res, next) => {
-        local_database['features'].push(req.body);
-        fs.writeFile(path.resolve(__dirname, '../'+temp_name_db), JSON.stringify(local_database), (err) => {
-            if (err){
-            console.log('wrong data format');
-            }
-            else{
-            console.log("data saved to 'database'.")
-            }
-        });
-        res.send(local_database);
-    });
-  
-    app.get('/test/', (req, res, next) => {
-        res.send("test_deploy")
-    });
+    }
+    res.send(result);
 });
+
+app.get('/test/', (req, res, next) => {
+    res.send("test_deploy")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server running.")
+})
